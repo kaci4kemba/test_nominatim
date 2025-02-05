d5e218b695d4df567095a73964bf63d89a25cee6
kaci4kemba <kycok111@gmail.com>
2025-02-05 15:45:20 +0300

    add logging

--- geo.py
+++ geo.py
@@ -1,25 +1,43 @@
 import requests
+import logging
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+    level=logging.DEBUG, filename=f"logs/{logger.name}.log",
+    format='%(levelname)s (%(asctime)s): %(message)s (Line: %(lineno)d) [%(filename)s]',
+    datefmt='%d/%m/%Y %I:%M:%S', filemode="w"
+    )
 
 url_base = "https://nominatim.openstreetmap.org/"
 headers = {'User-Agent': 'MyGeocodingApp/1.0 (myemail@example.com)'}
 
-def search(city):
-    url = f"{url_base}search?q={city}&format=json"
+
+def search(place):
+    logger.info(f"Делаем запрос используя {url_base} и {place}")
+    url = f"{url_base}search?q={place}&format=json"
     response = requests.get(url, headers=headers)
+    logger.info(f"Получаем ответ: {response.json}")
     data = response.json()
     if data:
+        logger.info(f"Вытаскиваем из data координаты")
         lat = data[0]['lat']
         lon = data[0]['lon']
         final = f"Широта: {lat}, Долгота: {lon}"
+        logger.info(f"Возвращаем полученные координаты {final}")
         return final
     else:
+        logger.info(f"Иначе, если не получили координаты")
         return "Город не найден"
 
 def reverse_search(lat, lon):
+    logger.info(f"Делаем запрос используя {url_base}, {lat} и {lon}")
     url = f"{url_base}reverse?format=json&lat={lat}&lon={lon}&zoom=18&addressdetails=0&dedupe=0"
     response = requests.get(url, headers=headers)
+    logger.info(f"Получаем ответ: {response.json}")
     data = response.json()
     if "error" in data:
+        logger.info(f"По переданным координатам ничего не нашли")
         return ("Город не найден.")
     else:
+        logger.info(f"Возвращаем полученную информацию о месте {data['display_name']}")
         return data["display_name"]
--- test_geo.py
+++ test_geo.py
@@ -2,21 +2,32 @@ import pytest
 import allure
 from geo import search, reverse_search
 from data import list_place, list_geo
+import logging
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+    level=logging.DEBUG, filename=f"logs/test_geo.log",
+    format='%(levelname)s (%(asctime)s): %(message)s (Line: %(lineno)d) [%(filename)s]',
+    datefmt='%d/%m/%Y %I:%M:%S', filemode="w"
+    )
 
 
 @pytest.mark.parametrize("place, coordinates", list_place)
-@allure.step("Проверка поиск координат по названию")
+@allure.story("Проверка поиск координат по названию")
 def test_search(place, coordinates):
     with allure.step(f"Поиск координат для '{place}'"):
+        logger.info(f"Ожидаемый результат: {place}")
         result = search(place)
         allure.attach(f"Ожидаемые: {coordinates}, \nПолученные: {result}", name="Результат", attachment_type=allure.attachment_type.TEXT)
         assert search(place) == coordinates
-
+logger.info("Тест прошел успешно!")
 
 @pytest.mark.parametrize("lat, lon, place", list_geo)
-@allure.step("Проверка поиска места по координатам")
+@allure.story("Проверка поиска места по координатам")
 def test_reverse_search(lat, lon, place):
     with allure.step(f"Поиск места по координатам ({lat}, {lon})"):
+        logger.info(f"Ожидаемый результат: {lat}, {lon}")
         result = reverse_search(lat, lon)
         allure.attach(f"Ожидаемые: {place}, \nПолученные: {result}", name="Результат", attachment_type=allure.attachment_type.TEXT)
         assert reverse_search(lat, lon) == place
+logger.info("Тест прошел успешно!")
